acfca2808929a949ebc3974b918b520bfd7c9980
saraPetrova01 <sara.petrova.m@gmail.com>
2023-01-19 12:40:40 +0200

    integers and etc

---
+++ firstlecture/fourthTask.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Tokens:key words ID' integers constants strings operators delimeters.";
+    cout << "Integers:are  fixed values,can be floating ,string literals,character literals. ";
+    cout << "Commands:are not part of the program they just control it's workflow.";
+}
